7b1788c201df97c5a6f8eb6da7ed6bd05a911a03
zjxjwxk <zjxjwxk@gmail.com>
2020-03-14 23:54:32 +0800

    add ignoreUrl

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/config/properties/JwtProperties.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/config/properties/JwtProperties.java
@@ -25,6 +25,8 @@ public class JwtProperties {
 
     private String md5Key = "randomKey";
 
+    private String ignoreUrl = "";
+
     public static String getJwtPrefix() {
         return JWT_PREFIX;
     }
@@ -67,5 +69,13 @@ public class JwtProperties {
 
     public void setMd5Key(String md5Key) {
         this.md5Key = md5Key;
+    }
+
+    public String getIgnoreUrl() {
+        return ignoreUrl;
+    }
+
+    public void setIgnoreUrl(String ignoreUrl) {
+        this.ignoreUrl = ignoreUrl;
     }
 }
