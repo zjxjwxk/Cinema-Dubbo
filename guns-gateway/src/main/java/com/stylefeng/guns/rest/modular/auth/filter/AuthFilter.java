c5c3a2294ff54d3f92cdc5def04f2bb1953c39d7
zjxjwxk <zjxjwxk@gmail.com>
2020-03-15 23:32:51 +0800

    override functions

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/filter/AuthFilter.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/filter/AuthFilter.java
@@ -2,6 +2,7 @@ package com.stylefeng.guns.rest.modular.auth.filter;
 
 import com.stylefeng.guns.core.base.tips.ErrorTip;
 import com.stylefeng.guns.core.util.RenderUtil;
+import com.stylefeng.guns.rest.common.CurrentUser;
 import com.stylefeng.guns.rest.common.exception.BizExceptionEnum;
 import com.stylefeng.guns.rest.config.properties.JwtProperties;
 import com.stylefeng.guns.rest.modular.auth.util.JwtTokenUtil;
@@ -55,6 +56,14 @@ public class AuthFilter extends OncePerRequestFilter {
         String authToken = null;
         if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
             authToken = requestHeader.substring(7);
+
+            // 通过token获取userID，并且将之存入ThreadLocal，以便后续业务调用
+            String userId = jwtTokenUtil.getUsernameFromToken(authToken);
+            if (userId == null) {
+                return;
+            } else {
+                CurrentUser.saveUserId(userId);
+            }
 
             //验证token是否过期,包含了验证jwt是否正确
             try {
