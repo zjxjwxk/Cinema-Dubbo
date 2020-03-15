4217d348692b813b48fda00cea173b6d2a9d536f
zjxjwxk <zjxjwxk@gmail.com>
2020-03-15 23:34:02 +0800

    override functions

--- guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserImpl.java
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserImpl.java
@@ -2,6 +2,8 @@ package com.stylefeng.guns.rest.modular.user;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.stylefeng.guns.api.user.UserAPI;
+import com.stylefeng.guns.api.user.UserInfoModel;
+import com.stylefeng.guns.api.user.UserModel;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,8 +11,28 @@ import org.springframework.stereotype.Component;
 public class UserImpl implements UserAPI {
 
     @Override
-    public boolean login(String username, String password) {
-        System.out.println("This is user service! " + username + ", " + password);
+    public int login(String username, String password) {
+        return 0;
+    }
+
+    @Override
+    public boolean register(UserModel userModel) {
         return false;
     }
+
+    @Override
+    public boolean checkUserName(String username) {
+        return false;
+    }
+
+    @Override
+    public UserInfoModel getUserInfo(int uuid) {
+        return null;
+    }
+
+    @Override
+    public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
+        return null;
+    }
+
 }
