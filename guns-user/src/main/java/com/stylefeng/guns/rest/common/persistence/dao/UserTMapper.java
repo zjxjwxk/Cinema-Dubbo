b3fa85d35b5451c4783355208b3dfb8ae369c73b
zjxjwxk <zjxjwxk@gmail.com>
2020-03-17 23:52:47 +0800

    add UserT Mapper and Entity

---
+++ guns-user/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/UserTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.UserT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author zjxjwxk
+ * @since 2020-03-17
+ */
+public interface UserTMapper extends BaseMapper<UserT> {
+
+}
