6cca10832f44e021e0a1370f4045734ebdb865a7
zjxjwxk <zjxjwxk@gmail.com>
2020-03-20 00:32:31 +0800

    implement Serializable

--- guns-api/src/main/java/com/stylefeng/guns/api/user/UserInfoModel.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/user/UserInfoModel.java
@@ -1,6 +1,8 @@
 package com.stylefeng.guns.api.user;
 
-public class UserInfoModel {
+import java.io.Serializable;
+
+public class UserInfoModel implements Serializable {
 
     private Integer uuid;
     private String username;
