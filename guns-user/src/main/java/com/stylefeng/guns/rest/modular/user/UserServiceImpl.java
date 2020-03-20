76f2272b8de85b53548da565de0c48904da5ebaa
zjxjwxk <zjxjwxk@gmail.com>
2020-03-20 23:57:10 +0800

    add service loadbalance

--- guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@Service(interfaceClass = UserAPI.class)
+@Service(interfaceClass = UserAPI.class, loadbalance="roundrobin")
 public class UserServiceImpl implements UserAPI {
 
     @Autowired
