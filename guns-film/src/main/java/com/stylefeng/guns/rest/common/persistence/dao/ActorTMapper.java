3ed43f2000637710c4d2368cf3cee570d959f7cc
zjxjwxk <zjxjwxk@gmail.com>
2020-03-22 23:18:10 +0800

    add entity, mapper of film module

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/ActorTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.ActorT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 演员表 Mapper 接口
+ * </p>
+ *
+ * @author zjxjwxk
+ * @since 2020-03-22
+ */
+public interface ActorTMapper extends BaseMapper<ActorT> {
+
+}
