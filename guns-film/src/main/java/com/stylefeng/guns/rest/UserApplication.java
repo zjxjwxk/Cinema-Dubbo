9e8f3a720c95a39b1c2df6ba6679f21fe4110b90
zjxjwxk <zjxjwxk@gmail.com>
2020-03-22 23:16:08 +0800

    add and init film module

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/UserApplication.java
@@ -0,0 +1,17 @@
+package com.stylefeng.guns.rest;
+
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zjxjwxk
+ */
+@SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
+@EnableDubboConfiguration
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
