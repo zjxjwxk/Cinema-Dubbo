42dddf65791c9a4a05203ae85994ef122eefc989
zjxjwxk <zjxjwxk@gmail.com>
2020-03-23 23:57:45 +0800

    call service interfaces in controller

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -1,5 +1,8 @@
 package com.stylefeng.guns.rest.modular.film;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.api.film.FilmServiceApi;
+import com.stylefeng.guns.rest.modular.film.vo.FilmIndexVO;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/film/")
 public class FilmController {
 
+    @Reference(interfaceClass = FilmServiceApi.class)
+    private FilmServiceApi filmServiceApi;
+
     /**
      * 获取首页信息接口
      * API网关：1. 功能聚合（API聚合）
@@ -20,23 +26,24 @@ public class FilmController {
      *                 2. 同一个接口对外暴露，降低了前后端分离开发的难度和复杂度
  *                缺点：
      *                 1. 一次获取数据过多，容易出现问题
-     * @return
+     * @return 响应VO
      */
     @RequestMapping(value = "getIndex", method = RequestMethod.GET)
     public ResponseVO getIndex() {
 
+        FilmIndexVO filmIndexVO = new FilmIndexVO();
         // 获取banner信息
-
+        filmIndexVO.setBanners(filmServiceApi.getBanners());
         // 获取热映的影片
-
+        filmIndexVO.setHotFilms(filmServiceApi.getHotFilms(true, 8));
         // 获取即将上映的影片
-
+        filmIndexVO.setSoonFilms(filmServiceApi.getSoonFilms(true, 8));
         // 获取票房排行榜
-
+        filmIndexVO.setBoxRanking(filmServiceApi.getBoxRanking());
         // 获取人气榜单
-
+        filmIndexVO.setExpectRanking(filmServiceApi.getExpectRanking());
         // 获取排行前100影片
-
-        return null;
+        filmIndexVO.setTop100(filmServiceApi.getTop());
+        return ResponseVO.success(filmIndexVO);
     }
 }
