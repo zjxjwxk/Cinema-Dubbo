1f8ecddb6ce4bd3b7017076382a3f6def1fd2cd6
zjxjwxk <zjxjwxk@gmail.com>
2020-03-24 00:34:26 +0800

    update Application name

--- guns-film/src/main/java/com/stylefeng/guns/rest/FilmApplication.java
+++ guns-film/src/main/java/com/stylefeng/guns/rest/FilmApplication.java
@@ -9,9 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
 @EnableDubboConfiguration
-public class UserApplication {
+public class FilmApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(FilmApplication.class, args);
     }
 }
