9b12f8fff9fa6c42d8f79d12e97bd56ad70d1bc1
zjxjwxk <zjxjwxk@gmail.com>
2020-04-01 23:44:32 +0800

    add comment

--- guns-api/src/main/java/com/stylefeng/guns/api/cinema/vo/HallInfoVO.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/cinema/vo/HallInfoVO.java
@@ -14,5 +14,8 @@ public class HallInfoVO implements Serializable {
     private String hallName;
     private String price;
     private String seatFile;
+    /**
+     * 已售座位必须关联订单表才能查询
+     */
     private String soldSeats;
 }
