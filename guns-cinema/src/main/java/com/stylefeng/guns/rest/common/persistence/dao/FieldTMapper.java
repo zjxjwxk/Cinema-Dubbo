cfbce654245229b96988f9c66038902c04a613a1
zjxjwxk <zjxjwxk@gmail.com>
2020-04-01 23:46:55 +0800

    add and implement getFilmInfoListByCinemaId, getHallInfo and getFilmInfoById Mapper xml

--- guns-cinema/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/FieldTMapper.java
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/FieldTMapper.java
@@ -1,7 +1,12 @@
 package com.stylefeng.guns.rest.common.persistence.dao;
 
+import com.stylefeng.guns.api.cinema.vo.FilmInfoVO;
+import com.stylefeng.guns.api.cinema.vo.HallInfoVO;
 import com.stylefeng.guns.rest.common.persistence.model.FieldT;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +18,9 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  */
 public interface FieldTMapper extends BaseMapper<FieldT> {
 
+    List<FilmInfoVO> getFilmInfoListByCinemaId(@Param("cinemaId") int cinemaId);
+
+    HallInfoVO getHallInfo(@Param("fieldId") int fieldId);
+
+    FilmInfoVO getFilmInfoById(@Param("fieldId") int fieldId);
 }
