56fb637009850b61d7298346e7549cd667fd2a97
zjxjwxk <zjxjwxk@gmail.com>
2020-04-07 13:03:52 +0800

    add OrderQueryVO

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/cinema/vo/OrderQueryVO.java
@@ -0,0 +1,14 @@
+package com.stylefeng.guns.api.cinema.vo;
+
+import lombok.Data;
+
+/**
+ * @author zjxjwxk
+ */
+@Data
+public class OrderQueryVO {
+
+    private String cinemaId;
+    private String filmId;
+    private String filmPrice;
+}
