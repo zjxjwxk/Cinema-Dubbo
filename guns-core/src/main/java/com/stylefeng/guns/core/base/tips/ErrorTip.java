5359e37436ec50c22086705eb8134e68bc20f8e2
zjxjwxk <zjxjwxk@gmail.com>
2020-04-07 13:05:35 +0800

    add status == 700 for token check

--- guns-core/src/main/java/com/stylefeng/guns/core/base/tips/ErrorTip.java
+++ guns-core/src/main/java/com/stylefeng/guns/core/base/tips/ErrorTip.java
@@ -1,16 +1,25 @@
 package com.stylefeng.guns.core.base.tips;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 返回给前台的错误提示
  *
  * @author fengshuonan
  * @date 2016年11月12日 下午5:05:22
  */
+@Slf4j
 public class ErrorTip extends Tip {
 
     public ErrorTip(int status, String message) {
         super();
-        this.status = 999;
-        this.message = "系统出现异常，请联系管理员";
+        if (status == 700) {
+            this.status = status;
+            this.message = message;
+        } else {
+            log.error("系统出现异常，请联系管理员，异常码：{}，异常信息：{}", status, message);
+            this.status = 999;
+            this.message = "系统出现异常，请联系管理员";
+        }
     }
 }
