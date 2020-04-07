9d8829fd64981c4671b9b471dfbbea62a872aeff
zjxjwxk <zjxjwxk@gmail.com>
2020-04-08 01:36:41 +0800

    add implements Serializable

--- guns-api/src/main/java/com/stylefeng/guns/api/cinema/vo/OrderQueryVO.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/cinema/vo/OrderQueryVO.java
@@ -2,11 +2,13 @@ package com.stylefeng.guns.api.cinema.vo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author zjxjwxk
  */
 @Data
-public class OrderQueryVO {
+public class OrderQueryVO implements Serializable {
 
     private String cinemaId;
     private String filmId;
