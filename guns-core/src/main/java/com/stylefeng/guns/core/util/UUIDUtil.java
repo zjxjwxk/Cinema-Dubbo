1ec01914d5d9f90a6313b374b3aa306c0b4fb0ad
zjxjwxk <zjxjwxk@gmail.com>
2020-04-07 13:05:55 +0800

    add UUIDUtil to generate UUID

---
+++ guns-core/src/main/java/com/stylefeng/guns/core/util/UUIDUtil.java
@@ -0,0 +1,13 @@
+package com.stylefeng.guns.core.util;
+
+import java.util.UUID;
+
+/**
+ * @author zjxjwxk
+ */
+public class UUIDUtil {
+
+    public static String genUuid() {
+        return UUID.randomUUID().toString();
+    }
+}
