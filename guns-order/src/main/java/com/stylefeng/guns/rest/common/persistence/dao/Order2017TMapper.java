d002ac9f9b863531984eb807d16d6ce6ec4030b9
zjxjwxk <zjxjwxk@gmail.com>
2020-04-09 23:21:36 +0800

    add Entity and Mapper of order_2017_t and order_2018_t

---
+++ guns-order/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/Order2017TMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.Order2017T;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 订单信息表 Mapper 接口
+ * </p>
+ *
+ * @author zjxjwxk
+ * @since 2020-04-09
+ */
+public interface Order2017TMapper extends BaseMapper<Order2017T> {
+
+}
---
+++ guns-order/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/Order2018TMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.Order2018T;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 订单信息表 Mapper 接口
+ * </p>
+ *
+ * @author zjxjwxk
+ * @since 2020-04-09
+ */
+public interface Order2018TMapper extends BaseMapper<Order2018T> {
+
+}
