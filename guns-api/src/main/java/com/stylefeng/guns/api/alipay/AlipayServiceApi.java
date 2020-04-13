00cc81f4436822980b5bba55ed6765cc6013aab1
zjxjwxk <zjxjwxk@gmail.com>
2020-04-14 00:35:44 +0800

    add comment

--- guns-api/src/main/java/com/stylefeng/guns/api/alipay/AlipayServiceApi.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/alipay/AlipayServiceApi.java
@@ -5,7 +5,17 @@ import com.stylefeng.guns.api.alipay.vo.AlipayResultVO;
 
 public interface AlipayServiceApi {
 
+    /**
+     * 预下单并获取二维码地址
+     * @param orderId 订单编号
+     * @return AlipayInfoVO
+     */
     AlipayInfoVO getQRCode(String orderId);
 
+    /**
+     * 获取订单状态
+     * @param orderId 订单编号
+     * @return AlipayResultVO
+     */
     AlipayResultVO getOrderStatus(String orderId);
 }
