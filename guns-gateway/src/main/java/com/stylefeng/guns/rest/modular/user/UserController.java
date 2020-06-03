83d5e1e5a841b25277d1dd385059e3bf40d704e8
zjxjwxk <zjxjwxk@gmail.com>
2020-06-03 20:57:44 +0800

    change mysql password

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zjxjwxk
  */
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
 @RequestMapping("/user/")
 public class UserController {
 
