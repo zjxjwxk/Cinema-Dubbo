4b6f296961df6b46d15b2ba7a885b11a1b01db33
zjxjwxk <zjxjwxk@gmail.com>
2020-06-06 00:15:47 +0800

    change CrossOrigin variable

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zjxjwxk
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
+@CrossOrigin
 @RequestMapping("/user/")
 public class UserController {
 
