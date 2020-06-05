c4b9ba4d9274c98fbf883b0edcf419f36caef27a
zjxjwxk <zjxjwxk@gmail.com>
2020-06-06 00:17:43 +0800

    add entrypoint.sh for each module

---
+++ guns-alipay/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+java -jar /cinema/workspace/guns-alipay-0.0.1.jar
+
+for (( ; ; ))
+do
+  sleep 5
+done
---
+++ guns-cinema/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+java -jar /cinema/workspace/guns-cinema-0.0.1.jar
+
+for (( ; ; ))
+do
+  sleep 5
+done
