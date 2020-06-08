8a26e88c0d6358751b0f75dacf82cad706a1b8eb
zjxjwxk <zjxjwxk@gmail.com>
2020-06-08 21:49:10 +0800

    update FTPUtil

--- guns-alipay/src/main/java/com/stylefeng/guns/rest/common/util/FTPUtil.java
+++ guns-alipay/src/main/java/com/stylefeng/guns/rest/common/util/FTPUtil.java
@@ -3,6 +3,7 @@ package com.stylefeng.guns.rest.common.util;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,47 +32,15 @@ public class FTPUtil {
             ftpClient.setControlEncoding("utf-8");
             ftpClient.connect(hostName, port);
             ftpClient.login(userName, password);
-            ftpClient.setDataTimeout(60000);
-            ftpClient.setConnectTimeout(60000);
+            int reply = ftpClient.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                log.error("FTP连接失败！");
+            } else {
+                log.info("FTP连接成功！");
+            }
         } catch (Exception e) {
             log.error("初始化FTP失败", e);
         }
-    }
-
-    /**
-     * 输入一个路径，将路径文件里的字符串返回
-     * @param fileAddress 文件路径
-     * @return 文件里的字符串
-     */
-    public String getFileStrByAddress(String fileAddress) {
-        BufferedReader bufferedReader = null;
-        try {
-            initFTPClient();
-            bufferedReader = new BufferedReader(
-                    new InputStreamReader(
-                            ftpClient.retrieveFileStream(fileAddress)
-                    )
-            );
-            StringBuilder stringBuilder = new StringBuilder();
-            while (true) {
-                String lineStr = bufferedReader.readLine();
-                if (lineStr == null) {
-                    break;
-                }
-                stringBuilder.append(lineStr);
-            }
-            ftpClient.logout();
-            return stringBuilder.toString();
-        } catch (Exception e) {
-            log.error("获取文件信息失败", e);
-        } finally {
-            try {
-                bufferedReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
     }
 
     /**
@@ -80,10 +49,13 @@ public class FTPUtil {
      * @param file 文件File
      * @return 是否上传成功
      */
-    public boolean  uploadFile(String fileName, File file) {
+    public boolean uploadFile(String fileName, File file) {
         FileInputStream fileInputStream = null;
         try {
             initFTPClient();
+            if (ftpClient == null) {
+                return false;
+            }
             fileInputStream = new FileInputStream(file);
 
             // FTP相关
